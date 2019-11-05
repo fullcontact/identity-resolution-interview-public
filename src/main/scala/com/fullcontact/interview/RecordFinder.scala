458d8a8fad8d60bac5e0968002c351bc7590636d
Aaron Stockton <aaron.stockton@rms.com>
2019-11-05 10:49:43 -0700

    writing out1/out2. testing dataframe operations

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -1,7 +1,71 @@
 package com.fullcontact.interview
 
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
 object RecordFinder {
+  val Identifier = "identifier"
+  val Query = "query"
+  val Record = "record"
+  val Records = "records"
+
   def main(args: Array[String]): Unit = {
-    // TODO: Implement your job here
+    val spark = SparkSession.builder()
+      .master("local[4]")
+      .getOrCreate()
+
+    val records = spark.read.csv("./Records.txt").toDF(Record).repartition(4)
+    val queries = spark.read.csv("./Queries.txt").toDF(Query).repartition(4)
+
+    val recordsById = explodeRecordsDF(records)
+
+    val withDupes = recordsById.join(queries, recordsById(Identifier) === queries(Query))
+      .select(queries(Query), recordsById(Record))
+
+    import spark.sqlContext.implicits._
+    withDupes
+      .map(row => {
+        s"${row.getAs(Query)}: ${row.getAs(Record)}"
+      })
+      .coalesce(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .text("Output1.txt")
+
+    val noDupes = groupRecordsByIdDF(withDupes)
+
+    noDupes
+      .map(row => s"${row.getAs(Identifier)}: ${row.getAs(Records)}")
+      .coalesce(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .text("Output2.txt")
   }
+
+  //(a b c) -> (a, a b c), (b, a b c), (c, a b c)
+  def explodeRecordsDF(recordsDF: DataFrame): DataFrame = {
+    import recordsDF.sqlContext.implicits._
+    recordsDF.as[String].mapPartitions(rows => {
+      val exploded = rows.flatMap(record => {
+        val identifiers: Array[String] = record.split(" ")
+        identifiers.map(id => (id, record))
+      })
+      exploded
+    }).toDF(Identifier, Record)
+  }
+
+  //(a, a b c), (a, c e f) -> (a, a b c e f)
+  def groupRecordsByIdDF(duped: DataFrame): DataFrame = {
+    import duped.sqlContext.implicits._
+    duped
+      .map(row => (row.getString(0), row.getString(1).split(" ")))
+      .toDF(Identifier, Records)
+      .groupBy(Identifier)
+      .agg(concat_ws(" ", array_distinct(flatten(collect_list(col(Records))))).alias(Records)
+      )
+  }
+
+
 }
+
+
--- src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
+++ src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
@@ -1,9 +1,94 @@
 package com.fullcontact.interview
 
-import org.scalatest.{FunSuite, Matchers}
+import org.apache.spark.sql.SparkSession
+import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
 
-class RecordFinderTest extends FunSuite with Matchers {
-  test("todo") {
-    pending
+class RecordFinderTest extends FunSuite with Matchers with BeforeAndAfter {
+  var sparkSession: SparkSession = _
+
+  test("Validate exploding records dataframe by each identifier") {
+    val spark = sparkSession
+
+    import spark.implicits._
+
+    val records = Seq(
+      "A B C",
+      "D E F",
+      "A C D"
+    ).toDF(RecordFinder.Record)
+
+    val expected = Seq(
+      ("A", "A B C"),
+      ("B", "A B C"),
+      ("C", "A B C"),
+      ("D", "D E F"),
+      ("E", "D E F"),
+      ("F", "D E F"),
+      ("A", "A C D"),
+      ("C", "A C D"),
+      ("D", "A C D"),
+    ).toDF(RecordFinder.Identifier, RecordFinder.Record)
+
+    val exploded = RecordFinder.explodeRecordsDF(records)
+
+    assert(
+      expected.as[(String, String)].collect() sameElements exploded.as[(String, String)].collect(),
+      "The exploded dataframe was not the same as the expected value"
+    )
+
   }
+
+  test("Validate grouping exploded dataframe by identifier") {
+    val spark = sparkSession
+    import spark.implicits._
+
+
+    val expected = Seq(
+      ("A", "A B C D"),
+      ("B", "A B C"),
+      ("C", "A B C D"),
+      ("D", "A C D E F"),
+      ("E", "D E F"),
+      ("F", "D E F")
+    ).toDS()
+      .as[(String, String)]
+      .map {
+        case (id: String, record: String) => {
+          (id, record.split(" ").sorted.mkString(" "))
+        }
+      }
+
+
+    val exploded = Seq(
+      ("A", "A B C"),
+      ("B", "A B C"),
+      ("C", "A B C"),
+      ("D", "D E F"),
+      ("E", "D E F"),
+      ("F", "D E F"),
+      ("A", "A C D"),
+      ("C", "A C D"),
+      ("D", "A C D"),
+    ).toDF(RecordFinder.Identifier, RecordFinder.Record)
+
+    val actual = RecordFinder.groupRecordsByIdDF(exploded)
+      .as[(String, String)]
+      .map {
+        case (id: String, record: String) => {
+          (id, record.split(" ").sorted.mkString(" "))
+        }
+      }.sort(org.apache.spark.sql.functions.asc("_1"))
+
+    assert(
+      expected.as[(String, String)].collect() sameElements actual.as[(String, String)].collect(),
+      "The exploded dataframe was not the same as the expected value"
+    )
+
+  }
+
+  before {
+    sparkSession = SparkSession.builder().master("local[4]").getOrCreate()
+  }
+
+
 }
