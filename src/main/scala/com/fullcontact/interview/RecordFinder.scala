290d023480e5268c680d97f56ffe4b8de85970cd
Brad Martin <brma7253@colorado.edu>
2021-07-27 14:46:14 -0600

    refactor query and record validation

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -1,4 +1,5 @@
 package com.fullcontact.interview
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object RecordFinder {
@@ -9,23 +10,42 @@ object RecordFinder {
       .setMaster("local")
     val sc = new SparkContext(conf)
 
-    // Reading info from Records.txt file (into arrays of strings) and Queries (into strings)
+    // Reading info RDDs from Records.txt file (into arrays of strings) and Queries (into strings)
     val recordsSplitRDD = sc.textFile("Records.txt")
       .map(l => l.split(" "))
-
     val queriesRDD = sc.textFile("Queries.txt")
 
-    // Do some stats/validation on input data (are all septuplets of ASCII 65-90?)
-    val non7UppersInRecords = recordsSplitRDD.map(sa => areWordsNot7Uppers(sa))
+    // Row counts/validation on input data (are all septuplets of ASCII 65-90? Do we have non-zero row counts?)
+    validateRecords(recordsSplitRDD)
+    validateQueries(queriesRDD)
+
+  }
+
+  def validateRecords(records: RDD[Array[String]]) : Unit = {
+    val non7UppersInRecords = records.map(sa => areWordsNot7Uppers(sa))
       .map(ia => ia.sum)
       .reduce((a, b) => a + b)
     println("Number of non-7-uppercase-letter words imported from Records.txt: " + non7UppersInRecords)
-    println("Number of records in Records.txt: " + recordsSplitRDD.count())
+    println("Number of records in Records.txt: " + records.count())
+    if (non7UppersInRecords > 0){
+      throw new RuntimeException("Non-7-uppercase-letter records found. Make sure all IDs are 7 uppercase letters.")
+    }
+    if (records.count() == 0) {
+      throw new RuntimeException("No records found from Records.txt. Please check path and data file.")
+    }
+  }
 
-    val non7UppersInQueries = queriesRDD.map(s => isWordNot7Uppers(s))
+  def validateQueries(queries: RDD[String]) : Unit = {
+    val non7UppersInQueries = queries.map(s => isWordNot7Uppers(s))
       .reduce((a, b) => a + b)
     println("Number of non-7-uppercase-letter words imported from Queries.txt: " + non7UppersInQueries)
-    println("Number of records in Queries.txt: " + queriesRDD.count())
+    println("Number of records in Queries.txt: " + queries.count())
+    if (non7UppersInQueries > 0){
+      throw new RuntimeException("Non-7-uppercase-letter queries found. Make sure all IDs are 7 uppercase letters.")
+    }
+    if (queries.count() == 0) {
+      throw new RuntimeException("No records found from Queries.txt. Please check path and data file.")
+    }
   }
 
   def areWordsNot7Uppers(sa: Array[String]) : Array[Int] = {
