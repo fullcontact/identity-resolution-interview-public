d89e8264a98d12d17b5eb4543244bf42324586a3
Brad Martin <brma7253@colorado.edu>
2021-07-28 16:11:40 -0600

    further refactor main functions and prep for tests

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -3,7 +3,7 @@ import java.io.File
 import java.nio.file.{Files, Paths}
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.functions._
 
@@ -11,14 +11,13 @@ import scala.collection.mutable
 import scala.reflect.io.Directory
 
 object RecordFinder {
+
   def main(args: Array[String]): Unit = {
     // Spark setup/init
     val conf = new SparkConf()
       .setAppName("BradsRecordFinder")
       .setMaster("local")
     val sc = new SparkContext(conf)
-    val spark: SparkSession = SparkSession.builder.master("local").getOrCreate
-    import spark.implicits._
 
     // Reading info RDDs from Records.txt file (into arrays of strings) and Queries (into strings)
     val recordsSplitRDD = sc.textFile("./Records.txt")
@@ -29,62 +28,12 @@ object RecordFinder {
     validateRecords(recordsSplitRDD)
     validateQueries(queriesRDD)
 
-    // Indexing records
-    val recordsSplitIndexedDF = spark.createDataFrame(recordsSplitRDD.zipWithIndex())
-      .withColumnRenamed("_1", "partialNeighborArray")
-      .withColumnRenamed("_2", "recordsIndex")
+    // Generate Output 1 DF that'll be used in both Output 1 text and Output 2 processing/text
+    val output1DF = generateOutput1DF(recordsSplitRDD, queriesRDD)
 
-    // Creating a flattened 'bridge' DF of (record index : ID) to join against Queries
-    val recordsIndicesFlatDF = recordsSplitIndexedDF.select($"recordsIndex", explode($"partialNeighborArray"))
-      .withColumnRenamed("col", "ID")
-
-    // Bringing Queries into a dataframe for joining into the tables above. MIGHT need to output ordered by query index.
-    val queriesDF = spark.createDataFrame(queriesRDD.zipWithIndex())
-      .withColumnRenamed("_1", "ID")
-      .withColumnRenamed("_2", "queryIndex")
-
-    // Getting pre-transforms version of Output1 (find what Records each ID was in)
-    val output1preTransformDF = queriesDF
-      .join(recordsIndicesFlatDF, queriesDF("ID") === recordsIndicesFlatDF("ID"), "inner")
-      .join(recordsSplitIndexedDF, recordsIndicesFlatDF("recordsIndex") === recordsSplitIndexedDF("recordsIndex"), "inner")
-      .select(
-        queriesDF("ID") as "ID",
-        recordsSplitIndexedDF("partialNeighborArray") as "partialNeighborArray"
-      )
-
-    // Transforming and outputting Output1 (clearing a path if necessary for an idempotent output)
-    if (Files.exists(Paths.get("./Output1.txt"))){
-      val dir1 = new Directory(new File("./Output1.txt"))
-      dir1.deleteRecursively()
-    }
-    val output1printout = output1preTransformDF
-      .withColumn("partialNeighborString", concat_ws(" ", col("partialNeighborArray")))
-      .select("ID", "partialNeighborString")
-      .rdd
-      .map(_.toString()
-        .replace(",", ": ")
-        .replace("[", "")
-        .replace("]", "")
-      )
-      .saveAsTextFile("./Output1.txt")
-
-    // Transform output1 neighbor arrays into sets, union them, and output deduped Output 2 (clearing a path)
-    if (Files.exists(Paths.get("./Output2.txt"))){
-      val dir2 = new Directory(new File("./Output2.txt"))
-      dir2.deleteRecursively()
-    }
-    val output2printout = output1preTransformDF
-      .rdd
-      .map(row => (row.get(0), row.get(1).asInstanceOf[mutable.WrappedArray[String]].toSet))
-      .reduceByKey(_ | _)
-      .map(_.toString()
-        .replace(",Set", ": ")
-        .replace("(", "")
-        .replace(")", "")
-        .replace(",", " ")
-      )
-      .saveAsTextFile("./Output2.txt")
-
+    // Transforming and outputting Output1 and Output2 (clearing a path if necessary for an idempotent output)
+    generateOutput1(output1DF)
+    generateOutput2(output1DF)
   }
 
   def validateRecords(records: RDD[Array[String]]) : Unit = {
@@ -133,5 +82,70 @@ object RecordFinder {
     }
   }
 
+  def generateOutput1DF(recordsSplitRDD: RDD[Array[String]], queriesRDD: RDD[String]): DataFrame = {
+    val spark: SparkSession = SparkSession.builder.master("local").getOrCreate
+    import spark.implicits._
 
+    // Indexing Records
+    val recordsSplitIndexedDF = spark.createDataFrame(recordsSplitRDD.zipWithIndex())
+      .withColumnRenamed("_1", "partialNeighborArray")
+      .withColumnRenamed("_2", "recordsIndex")
+
+    // Creating a flattened 'bridge' DF of (Record index : ID) to join against Queries
+    val recordsIndicesFlatDF = recordsSplitIndexedDF.select($"recordsIndex", explode($"partialNeighborArray"))
+      .withColumnRenamed("col", "ID")
+
+    // Bringing Queries into a DF for joining into the tables above.
+    val queriesDF = spark.createDataFrame(queriesRDD.zipWithIndex())
+      .withColumnRenamed("_1", "ID")
+      .withColumnRenamed("_2", "queryIndex")
+
+    // Getting DF version of Output1 (find what - 0:Many - Records array each Query ID was in)
+    val output1DF = queriesDF
+      .join(recordsIndicesFlatDF, queriesDF("ID") === recordsIndicesFlatDF("ID"), "inner")
+      .join(recordsSplitIndexedDF, recordsIndicesFlatDF("recordsIndex") === recordsSplitIndexedDF("recordsIndex"), "inner")
+      .select(
+        queriesDF("ID") as "ID",
+        recordsSplitIndexedDF("partialNeighborArray") as "partialNeighborArray"
+      )
+
+    return output1DF
+  }
+
+  def generateOutput1(df: DataFrame): Unit = {
+    if (Files.exists(Paths.get("./Output1.txt"))){
+      val dir1 = new Directory(new File("./Output1.txt"))
+      dir1.deleteRecursively()
+    }
+
+    df
+      .withColumn("partialNeighborString", concat_ws(" ", col("partialNeighborArray")))
+      .select("ID", "partialNeighborString")
+      .rdd
+      .map(_.toString()
+        .replace(",", ": ")
+        .replace("[", "")
+        .replace("]", "")
+      )
+      .saveAsTextFile("./Output1.txt")
+  }
+
+  def generateOutput2(df: DataFrame): Unit = {
+    if (Files.exists(Paths.get("./Output2.txt"))){
+      val dir2 = new Directory(new File("./Output2.txt"))
+      dir2.deleteRecursively()
+    }
+
+    df
+      .rdd
+      .map(row => (row.get(0), row.get(1).asInstanceOf[mutable.WrappedArray[String]].toSet))
+      .reduceByKey(_ | _)
+      .map(_.toString()
+        .replace(",Set", ": ")
+        .replace("(", "")
+        .replace(")", "")
+        .replace(",", " ")
+      )
+      .saveAsTextFile("./Output2.txt")
+  }
 }
