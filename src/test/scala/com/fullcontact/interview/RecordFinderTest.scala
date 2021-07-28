972770248e52a07346b97589fa0a636b52b34fbc
Brad Martin <brma7253@colorado.edu>
2021-07-28 17:06:53 -0600

    test array-of-7-letter-strings validation

--- src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
+++ src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
@@ -1,7 +1,6 @@
 package com.fullcontact.interview
 
 import org.scalatest.{FunSuite, Matchers}
-import com.fullcontact.interview.RecordFinder
 
 class RecordFinderTest extends FunSuite with Matchers {
   test("Proper validation of an ID having/not having 7 uppercase letters") {
@@ -9,5 +8,19 @@ class RecordFinderTest extends FunSuite with Matchers {
     assert(RecordFinder.isWordNot7Uppers("ABCDEFg") == 1)
     assert(RecordFinder.isWordNot7Uppers("ABCD FG") == 1)
     assert(RecordFinder.isWordNot7Uppers("1BCDEFG") == 1)
+  }
+
+  test("Proper validation of # non-7-uppercase-letter words in array") {
+    val goodArr = Array("ABCDEFG", "BCDEFGH", "ZYXWVUT")
+    val badArr1 = Array("ABCDEFG", "BCDEFGH", "ZYXWVU1")
+    val badArr2 = Array("ABCDEFG", "BCDEFG", "ZYXWVUT")
+    val badArr3 = Array("aBCDEFG", "BCDEFGH", "ZYXWVUT")
+    val badArr4 = Array("aBCDEFG", "BCDEFG1", "ZYXWVUT", "")
+
+    assert(RecordFinder.areWordsNot7Uppers(goodArr).sum == 0)
+    assert(RecordFinder.areWordsNot7Uppers(badArr1).sum == 1)
+    assert(RecordFinder.areWordsNot7Uppers(badArr2).sum == 1)
+    assert(RecordFinder.areWordsNot7Uppers(badArr3).sum == 1)
+    assert(RecordFinder.areWordsNot7Uppers(badArr4).sum == 3)
   }
 }
