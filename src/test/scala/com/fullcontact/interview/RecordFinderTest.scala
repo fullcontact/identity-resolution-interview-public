efa7e24778b2e05368543989f58356f49c852327
Brad Martin <brma7253@colorado.edu>
2021-07-28 16:57:44 -0600

    test 7 uppercase letter validation

--- src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
+++ src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
@@ -1,9 +1,13 @@
 package com.fullcontact.interview
 
 import org.scalatest.{FunSuite, Matchers}
+import com.fullcontact.interview.RecordFinder
 
 class RecordFinderTest extends FunSuite with Matchers {
-  test("todo") {
-    pending
+  test("Proper validation of an ID having/not having 7 uppercase letters") {
+    assert(RecordFinder.isWordNot7Uppers("ABCDEFG") == 0)
+    assert(RecordFinder.isWordNot7Uppers("ABCDEFg") == 1)
+    assert(RecordFinder.isWordNot7Uppers("ABCD FG") == 1)
+    assert(RecordFinder.isWordNot7Uppers("1BCDEFG") == 1)
   }
 }
