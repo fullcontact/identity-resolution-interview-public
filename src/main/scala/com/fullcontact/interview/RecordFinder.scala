dd5117422a5a744f3fbfe4f9ac0396242977f26c
Peter-John Rowe <rowe.peterjohn@gmail.com>
2022-04-04 13:01:40 -0600

    write reduced recordsByQuery

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -1,7 +1,7 @@
 package com.fullcontact.interview
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession, functions => SQLFunc}
 
 object RecordFinder {
   def main(args: Array[String]): Unit = {
@@ -18,13 +18,33 @@ object RecordFinder {
       .textFile("Queries.txt")
       .withColumnRenamed("value", "query")
 
-    queries.show(5, truncate = false)
-
     val records: DataFrame = sparkSession.read
       .textFile("Records.txt")
       .map(x => x.split("\\W+"))
       .withColumnRenamed("value", "recordIds")
 
-    records.show(5, truncate = false)
+    val recordsByQuery: DataFrame  = queries.join(records, SQLFunc.array_contains(records("recordIds"), queries("query")))
+
+    recordsByQuery.map(row => (row.getString(0), row.getSeq[String](1).mkString(" ")))
+      .write
+      .mode(SaveMode.Overwrite)
+      .option("delimiter", ":")
+      .csv("Output1.txt")
+
+    val reducedRecordsByQuery: DataFrame = recordsByQuery.groupBy($"query")
+      .agg(
+        SQLFunc.concat_ws(" ",
+          SQLFunc.array_distinct(
+            SQLFunc.flatten(
+              SQLFunc.collect_list($"recordIds")
+            )
+          )
+        )
+      )
+
+    reducedRecordsByQuery.write
+      .mode(SaveMode.Overwrite)
+      .option("delimiter", ":")
+      .csv("Output2.txt")
   }
 }
