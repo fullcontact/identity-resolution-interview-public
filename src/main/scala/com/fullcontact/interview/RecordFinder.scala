e1e66c3b62ed5a2946a674128f18d1dff019e2b8
Peter-John Rowe <rowe.peterjohn@gmail.com>
2022-04-04 12:23:13 -0600

    read query records from file

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -1,7 +1,23 @@
 package com.fullcontact.interview
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 object RecordFinder {
   def main(args: Array[String]): Unit = {
-    // TODO: Implement your job here
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sparkSession: SparkSession = SparkSession.builder()
+      .appName("Record Builder")
+      .master("local[*]")
+      .getOrCreate()
+
+    import sparkSession.implicits._
+
+    val queries: DataFrame = sparkSession.read
+      .textFile("Queries.txt")
+      .withColumnRenamed("value", "query")
+
+    queries.show(5, false)
   }
 }
