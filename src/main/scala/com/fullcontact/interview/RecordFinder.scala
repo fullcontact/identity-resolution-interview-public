9970025b11f39be870335108e0d9cfa4ebe9423e
Joshua Michel <joshi.michel@gmail.com>
2023-03-30 21:09:02 -0400

    Break into functions and add tests for each.

--- src/main/scala/com/fullcontact/interview/RecordFinder.scala
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -1,7 +1,9 @@
 package com.fullcontact.interview
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions.{col, collect_list}
+
+case class Record(id: String, associated: Seq[String])
 
 /**
  * Solution pattern
@@ -17,46 +19,84 @@ object RecordFinder {
     import spark.implicits._
 
     //Read the records
-    val records = spark.read.text("Records.txt")
-
-    //Tag each element of the record with the full record
-    val tagged = records.flatMap(r => {
-      val value = r.getString(0)
-      val array = value.split(" ").toSeq
-      array.map(ele => (ele, array))
-    }).toDF("id", "associated").repartition(col("id")).cache()
+    val records = spark.read.text("Records.txt").as[String]
 
     //Get the queries
-    val queries = spark.read.text("Queries.txt").repartition(col("value"))
+    val queries = spark.read.text("Queries.txt").repartition(col("value")).as[String]
+
+    //Tag each element of the record with the full record
+    val allRecords = parseRecords(records).repartition(col("id")).cache()
 
     //Run the query
-    val found = queries.join(tagged, queries("value") === tagged("id"), "inner")
-      .select("id", "associated")
+    val qr1 = runQuery(allRecords, queries)
+    makeForOutput(qr1).coalesce(1).write.mode("overwrite").text("Output1.txt")
 
-    println(found.count()) //32314 matches
+    //Run the query and aggregate by id
+    val qr2 = runQueryAgg(allRecords, queries)
+    makeForOutput(qr2).coalesce(1).write.mode("overwrite").text("Output2.txt")
+  }
 
-    //Take each match and return the values
-    val output1 = found.map(r => {
-      val id = r.getString(0)
-      val ids = r.getAs[Seq[String]](1)
+  /**
+   * Take a string of space separated records and construct a Record for each element with the whole set as associated
+   * @param df the set of string inputs
+   * @return a set of Records
+   */
+  def parseRecords(df: Dataset[String]): Dataset[Record] = {
+    import df.sparkSession.implicits._
 
-      s"$id: ${ids.mkString(" ")}"
+    df.flatMap(str => {
+      val array = str.split(" ").toSeq
+      array.map(ele => Record(ele, array))
     })
+  }
 
-    output1.coalesce(1).write.mode("overwrite").text("Output1.txt")
+  /**
+   * Run the query against all the records and return every matching record for each query
+   * @param records The set of records
+   * @param queries The set of queries
+   * @return the query results
+   */
+  def runQuery(records: Dataset[Record], queries: Dataset[String]): Dataset[Record] = {
+    import records.sparkSession.implicits._
 
-    //Take each match, collect the id sets, reduce to unique values in a parallel manner
-    //Alternative would be to do the the uniqueness pass with a UDAF
-    val output2 = found.groupBy(col("id"))
-      .agg(collect_list("associated").as("allAssociated"))
+    records.joinWith(queries, records("id") === queries("value"), "inner").map(_._1)
+  }
+
+  /**
+   * Run the query against all the records and return a list of every unique matching record value for each query
+   *
+   * This is implemented in a phased manner where the matches are collected and the a uniqueness pass is applied.
+   * An alternative solution would be to due the uniqueness during aggregation with a UDAF.
+   * Also to note this does not make use of caching if the runQuery result is also needed.
+   * @param records The set of records
+   * @param queries The set of queries
+   * @return the query results
+   */
+  def runQueryAgg(records: Dataset[Record], queries: Dataset[String]): Dataset[Record] = {
+    import records.sparkSession.implicits._
+
+    runQuery(records, queries)
+      .groupBy(col("id"))
+      .agg(collect_list("associated").as("associated"))
       .map(r => {
         val id = r.getString(0)
         val idSets = r.getAs[Seq[Seq[String]]](1)
         val ids = idSets.map(_.toSet).reduce(_.union(_)) //Union to remove uniques
 
-        s"$id: ${ids.mkString(" ")}"
+        Record(id, ids.toSeq)
       })
+  }
 
-    output2.coalesce(1).write.mode("overwrite").text("Output2.txt")
+  /**
+   * Prepare the QueryResults for output
+   * @param df the query results
+   * @return a string of format: `(id): (id-match-1) ... (id-match-n)`
+   */
+  def makeForOutput(df: Dataset[Record]): Dataset[String] = {
+    import df.sparkSession.implicits._
+
+    df.map(r => {
+      s"${r.id}: ${r.associated.mkString(" ")}"
+    })
   }
 }
--- src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
+++ src/test/scala/com/fullcontact/interview/RecordFinderTest.scala
@@ -1,9 +1,93 @@
 package com.fullcontact.interview
 
-import org.scalatest.{FunSuite, Matchers}
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
-class RecordFinderTest extends FunSuite with Matchers {
-  test("todo") {
-    pending
+class RecordFinderTest extends FlatSpec with Matchers with BeforeAndAfterAll {
+
+  //All lazy vals to delay creation until tests run
+  lazy val spark: SparkSession = SparkSession.builder()
+    .master("local[*]")
+    .appName("Identity Resolution")
+    .getOrCreate()
+
+  lazy val recordSrcDf : Dataset[String] = {
+    import spark.implicits._
+    Seq(
+      "a b c",
+      "d e",
+      "a z"
+    ).toDS()
+  }
+
+  lazy val queryDf: Dataset[String] = {
+    import spark.implicits._
+    Seq(
+      "a",
+      "d",
+      "z"
+    ).toDS()
+  }
+
+  lazy val recordsDf: Dataset[Record] = {
+    import spark.implicits._
+    Seq(
+      Record("a", Seq("a", "b", "c")), Record("b", Seq("a", "b", "c")), Record("c", Seq("a", "b", "c")),
+      Record("d", Seq("d", "e")), Record("e", Seq("d", "e")),
+      Record("a", Seq("a", "z")), Record("z", Seq("a", "z")),
+    ).toDS()
+  }
+
+  lazy val queryRecords1Df: Dataset[Record] = {
+    import spark.implicits._
+    Seq(
+      Record("a", Seq("a", "b", "c")),
+      Record("d", Seq("d", "e")),
+      Record("a", Seq("a", "z")),
+      Record("z", Seq("a", "z"))
+    ).toDS()
+  }
+
+  lazy val queryRecords2Df: Dataset[Record] = {
+    import spark.implicits._
+    Seq(
+      Record("a", Seq("a", "b", "c", "z")),
+      Record("d", Seq("d", "e")),
+      Record("z", Seq("a", "z"))
+    ).toDS()
+  }
+
+  lazy val queryOutput2 : Dataset[String] = {
+    import spark.implicits._
+    Seq(
+      "a: a b c z",
+      "d: d e",
+      "z: a z"
+    ).toDS()
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    spark.stop()
+  }
+
+  "parseRecords" should "convert id groups into relationships of each id ot the whole group" in {
+    val res = RecordFinder.parseRecords(recordSrcDf)
+    res.collect() should contain theSameElementsAs recordsDf.collect()
+  }
+
+  "runQuery" should "query the records and pull the relevant records" in {
+    val res = RecordFinder.runQuery(recordsDf, queryDf)
+    res.collect() should contain theSameElementsAs queryRecords1Df.collect()
+  }
+
+  "runQueryAgg" should "query the records and pull the relevant records" in {
+    val res = RecordFinder.runQueryAgg(recordsDf, queryDf)
+    res.collect() should contain theSameElementsAs queryRecords2Df.collect()
+  }
+
+  "makeForOutput" should "prepare the records for output by formatting them" in {
+    val res = RecordFinder.makeForOutput(queryRecords2Df)
+    res.collect() should contain theSameElementsAs queryOutput2.collect()
   }
 }
